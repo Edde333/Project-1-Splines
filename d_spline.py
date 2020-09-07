0bec471560220c7a9c91f0317920319ffe75adf9
Adam Larssona <kaddeadam@gmail.com>
2020-09-07 14:59:46 +0200

    d_spline made object oriented

--- d_spline.py
+++ d_spline.py
@@ -7,78 +7,97 @@ Created on Fri Sep  4 10:47:42 2020
 
 @author: kadde
 """
-def expandArray(u, k):
-    u = np.insert(u, 0, np.ones(k-1)*u[0])
-    u = np.append(u,np.ones(k-1)*u[-1])
-    return u
 
-def findHotInterval(u,x):
-    """
-    I = np.argmax(u > x) - 1
-    if I < k-1: return k-1
-    """
-    I = np.zeros(len(x), dtype = 'int8')
-    for i in range(len(x)):
-        index = np.argmax(u >= x[i]) - 1
-        if index < k-1: index = k-1
-        I[i] = index
-    return I
+class d_spline:
+    def __init__(self, k, u, d):
+        self.k = 3
+        self.u = u
+        self.d = d
+       
+    
+    def dSpline(self,x):
+        self.u = self.expandArray(self.u)
+        ret = np.zeros((self.d.ndim,x.shape[0]))
+        I = self.findHotInterval(self.u,x)
+        # Run dSpline for each row(dimension) of the d matrix.
+        if self.d.ndim > 1:
+            for dimension in range(self.d.ndim):
+                currd = self.expandArray(self.d[dimension])
+                left = I
+                right = I+1
+                depth = 0
+                ret[dimension] = self.dRecursive(x, I, currd, depth, left, right)
+        else: 
+            self.d = self.expandArray(self.d)
+            left = I
+            right = I+1
+            depth = 0
+            return self.dRecursive(x, I, self.d, depth, left, right)
+        return ret
 
+    def expandArray(self,u):
+        u = np.insert(u, 0, np.ones(self.k-1)*u[0])
+        u = np.append(u,np.ones(self.k-1)*u[-1])
+        return u
+    
+    def findHotInterval(self,u,x):
+        """
+        I = np.argmax(u > x) - 1
+        if I < k-1: return k-1
+        """
+        I = np.zeros(len(x), dtype = 'int8')
+        for i in range(len(x)):
+            index = np.argmax(u >= x[i]) - 1
+            if index < self.k-1: index = self.k-1
+            I[i] = index
+        return I
+    
+        
+        
+    def dRecursive(self, x, I, d, depth, left, right):
+        if np.any(right-left == self.k+1):
+            return d[left+1]
+        a = self.alpha(x,left, right)
+        d0 = self.dRecursive(x, I, d,  depth + 1, left - 1, right)
+        d1 = self.dRecursive(x, I, d, depth + 1, left, right + 1)
+        return a*d0 + (1-a)*d1
+        
+        
+        
+    def alpha(self, x,left, right):
+        alphdivider = (self.u[right]-self.u[left])
+        zeroValues = np.where(alphdivider == 0)
+        alphdivider[zeroValues] = 1
+        alph = (self.u[right] - x)/alphdivider
+        alph[zeroValues] = 0
+        return alph
     
     
-def dRecursive(x, I, depth, left, right):
-    if np.any(right-left == k+1):
-        return d[left+1]
-    a = alpha(x,left, right)
-    d0 = dRecursive(x, I, depth + 1, left - 1, right)
-    d1 = dRecursive(x, I, depth + 1, left, right + 1)
-    return a*d0 + (1-a)*d1
     
-    
-    
-def alpha(x,left, right):
-    return (u[right] - x)/(u[right]-u[left])
 
 
-def dSpline(x):
-    I = findHotInterval(u,x)
-    left = I
-    right = I+1
-    depth = 0
-    y = dRecursive(x, I, depth, left, right)
-    return y
+# k = 3
+# u = np.array([0.5, 2, 5, 8, 9, 11])
+# d = np.array([4, 0.5, 3.4, 5.7, 5,2])
 
+# x = np.array([7])
+# u = expandArray(u,k)
+# d = expandArray(d,k)
+# I = findHotInterval(u,x)
+# left = I
+# right = I+1
+# depth = 0
 
-k = 3
-u = np.array([1, 2, 3, 4, 5, 6, 7])
-d = np.array([1, 3, 3,1, 0,0,1])
+# y = dRecursive(x, I, depth, left, right)
+# print(y)
 
+# f = lambda x: dSpline(x)
 
+# x = sp.linspace(1,5,100)
 
-x = np.array([5])
-u = expandArray(u,k)
-d = expandArray(d,k)
-I = findHotInterval(u,x)
-left = I
-right = I+1
-depth = 0
-
-y = dRecursive(x, I, depth, left, right)
-print(y)
-
-
-
-
-
-
-
-f = lambda x: dSpline(x)
-
-x = sp.linspace(1,7,100)
-
-plt.plot(x,f(x))
-plt.scatter(u,d)
-plt.show()
+# plt.plot(x,f(x))
+# plt.scatter(u,d)
+# plt.show()
 
 
 
