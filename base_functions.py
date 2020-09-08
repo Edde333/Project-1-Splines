65baf7135429e678118b15759a6f10c78189b4e0
oliviaarnfalk <olivia.arnfalk@gmail.com>
2020-09-08 12:40:29 +0200

    Changes

--- base_functions.py
+++ base_functions.py
@@ -1,16 +1,45 @@
 from  scipy import *
-from  pylab import *
+from  pylab import * 
 import numpy as np
+
+
+"""
+
+"""
+
 
 def getBaseFunc(u_real, i, k = 3):
     # Solves fact that extra knots seems to be needed but are not
     u = u_real.copy()
+    if i > len(u_real) - 3 or i < 0:
+        raise Exepction("Wrong input")
+    
     for j in range(k-2):
         u = np.append(u,1)
     return getBaseFuncRec(u,i,k)
 
+
+
 # Recursive algorithm to find base function
-def getBaseFunc(u, i, k = 3):
+def getBaseFuncRec(u, i, k = 3):
+    """
+    Recursive method that finds the base function corresponding with 
+    a certain index in the u-vector
+
+
+    Parameters 
+    --------
+    u : array of floats, nodes in u_i
+    i : integer, the index of the relevant node u_i
+    k : integer, the degree of the desired polynomial, is decreased with every iteration 
+
+
+    Returns 
+    -------
+    Function (x)
+        The base function as a polynomial of degree k
+    
+    """  
     if k == 0:
         if u[i-1] == u[i]:
             return lambda x: 0
@@ -38,19 +67,46 @@ def getBaseFunc(u, i, k = 3):
         
         return lambda x: factor1(x) * getBaseFuncRec(u, i, k-1)(x) + factor2(x) * getBaseFuncRec(u, i+1, k-1)(x)
 
-# Returns s(x)
+
+
+
 def getCubicSpline(x, u, d):
-    # Checks for correct x's
-    if (x > u[-3] + x < u[2]).any():
+    """
+    Uses the deBoor algorithm to create splines
+    
+    Parameters
+    ----------
+    x : array of floats, array of floats
+        the point(s) containd in u, for which the alogorithm computes a spline
+    u : array of floats, length K, nodes in u_i
+    d : coordinates corresponding to nodes u_1 to u_K-1 
+
+
+    Raises
+    ------
+    Exception "Wrong input"
+        if any index x lays outside the bounds of u
+
+    Returns
+    -------
+    (s(z), s(y))
+        Tuple with coordinates for point(s) x
+    """
+    
+    I = np.array([]) 
+    # Checks if all points are contained within u
+    if (x > u[-3]).any() or (x < u[2]).any():
         raise Exception("Wrong input")
+    
         
-    # Finds and saves all base functions
+    # Finds and saves all base functions in array base_functions
     base_functions = np.array([])
     for i in range(len(u)-2):
-        base_functions = np.append(base_functions, getBaseFunc(u, i)
-        
+        base_functions = np.append(base_functions, getBaseFunc(u, i))
+    
+                                   
     # Finds hot intervals
-    I = np.array([])
+    I = np.array([]) 
     for i in range(len(x)):
         for j in range(len(u)):
             if (x[i] >= u[j]) and (x[i] < u[j+1]):
@@ -61,15 +117,39 @@ def getCubicSpline(x, u, d):
     #         I = something
     
     for i in range(len(x)):
-        control_points_x = np.array([])
+        control_points_z = np.array([])
         control_points_y = np.array([])
         for j in range(4):
-            control_points_x = np.append(control_points_x, d[(I[i]-2) + j][0])
+            control_points_z = np.append(control_points_z, d[(I[i]-2) + j][0])
             control_points_y = np.append(control_points_y, d[(I[i]-2) + j][1])
         hot_base_functions = base_functions[I[i]-1 : I[i]+3]
-        s_x = control_points_x @ hot_base_functions
+        s_z = control_points_z @ hot_base_functions
         s_y = control_points_y @ hot_base_functions
+
     
     
+    return zip(s_z, s_y)
+
+
+
+
+def baseFuncSum(x, u):
+    """
+    Returns the sum of all base functions in points x
+
+    Parameters
+    ----------
+    x : array of floats, points for which the algorithm should work
+    u : array of floats, nodes in u_i
+
+    Returns
+    -------
+    array of floats, sum of all base functions in the given points x
+
+    """
+    base_functions = np.array([])
+    for i in range(len(u)-2):
+        base_functions = np.append(base_functions, getBaseFunc(u, i))
     
-    return zip(s_x, s_y)
+                                   
+    return np.sum(basefunctions(x))
