edee5f15162cc0f4e35ab469deccbbf927dd2d6b
OArnfalk <64954327+OArnfalk@users.noreply.github.com>
2020-09-24 09:25:24 +0200

    Update line_search.py
    Added method: get_acceptable_line_search():
    Added conditions for rho and sigma

--- Project 2 - Optimization/line_search.py
+++ Project 2 - Optimization/line_search.py
@@ -3,33 +3,67 @@ import scipy as sp
 from scipy import optimize
 
 """
-Method to find an appropriate step size alpha 
+Method to find an appropriate step size alpha, using the provided line search method
+
+
+
 Parameters
 ----------
 f   :   function 
+
 g   :   function, the gradient of f
+
 xk  :   np array, dtype = float, the active coordinates
+
 sk  :   np array, dtype = float, direction for minimization given coordinates xk
+
 line_search_method   : string, specifies which line search method to be used, 
                         accepts "exact" and "inexact", default value "exact"
                         
 line_searc_condition : string, specifies which conditions to be used for 
                         inexact line search, accepts "goldstein" and "WP", 
                         default value "goldstein"
+
 a0  :   float, current step size, default value 1             
-rho :   float, constant 
-sigma : float, constant
+
+rho :   float, constant acceptable values between 0 and 0.5
+        default value 0.1
+
+sigma : float, constant acceptable values between 0 and 1, sigma >= rho
+        default value 0.7
+
 tau :   float, constant
+        default value 0.1
+
 chi :   float, constant
+        default value 9
+
+
+
+Raises
+------
+    ValueError
+        Raises a ValueError if rho or sigma are unacceptable input values
+    Invalid line search method
+    
+
 Returns
 -------
 a0 : float
         Step size 
 """
+
+
 def line_search(f, g, xk, sk, line_search_method = "exact", line_search_condition = "goldstein", 
-                a0 = 100, rho = 0.1, sigma = 0.7, tau = 0.1, chi = 9):
+                a0 = 1, rho = 0.1, sigma = 0.7, tau = 0.1, chi = 9):
+    
+    
+    
+    
     if line_search_method == 'none':
         return 1.
+    
+    
     
     elif line_search_method == "exact":
         fa = lambda a: f(np.add(xk, a * sk))    
@@ -41,6 +75,12 @@ def line_search(f, g, xk, sk, line_search_method = "exact", line_search_conditio
 
 
     elif line_search_method == "inexact":
+        
+        if (rho < 0 or rho > 0.5):
+            raise ValueError("Input rho out of bounds, acceptable values 0 < rho < 0.5")
+        if (sigma < 0 or sigma > 1 or sigma < rho):
+            raise ValueError("Input rho out of bounds, acceptable values 0 < sigma < 1 with sigma >= rho")
+        
         aL = 0
         aU = 10**99
         a0k = (np.add(xk, a0 * sk))
@@ -90,12 +130,10 @@ def line_search(f, g, xk, sk, line_search_method = "exact", line_search_conditio
     
 """
 Method to check if the stepsize a0 fulfills the inexact line search conditions
+
+
 Parameters
 ----------
-f   :   function 
-g   :   function, the gradient of f
-xk  :   np array, dtype = float, the active coordinates
-sk  :   np array, dtype = float, direction for minimization given coordinates xk
                         
 line_searc_condition : string, specifies which conditions to be used for 
                         inexact line search, accepts "goldstein" and "WP"
@@ -106,6 +144,18 @@ rho :   float, constant
 sigma : float, constant
 tau :   float, constant
 chi :   float, constant
+fa0 :   float, function value given step size a0
+faL :   float, function value given step size aL
+ga0 :   float, gradient value given step size a0
+gaL :   float, gradient value given step size aL
+
+
+
+Raises
+------
+Invalid line search condition
+
+
 Returns
 -------
 [LC, RC] : list of booleans
@@ -138,7 +188,16 @@ def get_conditions(line_search_condition, a0, aL, aU, rho, sigma, tau, chi, fa0,
 
 
 """
-Method to calculate da0, used in inexact line search
+Method to calculate the extrapolated da0, used in inexact line search
+
+
+Parameters
+----------
+a0  :   float, current step size  
+aL  :   float, lower end of step size interval  
+ga0 :   float, gradient value given step size a0
+gaL :   float, gradient value given step size aL 
+
 Returns 
 -------
 da0 : float 
@@ -152,7 +211,16 @@ def extrapolate(a0, aL, ga0, gaL):
 
 
 """
-Method to calculate abar, used in inexact line search
+Method to calculate the interpolated abar, used in inexact line search
+
+Parameters
+----------
+a0  :   float, current step size  
+aL  :   float, lower end of step size interval  
+fa0 :   float, function value given step size a0
+faL :   float, function value given step size aL
+gaL :   float, gradient value given step size aL 
+
 Returns 
 -------
 abar : float 
@@ -161,4 +229,21 @@ def interpolate(a0, aL, fa0, faL, gaL):
 
     return (a0 - aL)**2 * gaL / (2 * (faL - fa0 + (a0 - aL) * gaL))
 
-  
+
+
+"""
+Method to return acceptable line search methods, and acceptable 
+line search conditions for inexact line search
+
+Returns
+-------
+acceptable_list_methods, acceptable_list_conditions
+"""
+
+def get_acceptable_line_search():
+    acceptable_list_methods = [ "exact",
+            "inexact",
+            "none",
+            ]
+    acceptable_list_conditions = ["goldstein", "WP"]
+    return acceptable_list_methods, acceptable_list_conditions
